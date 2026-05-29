68a99656ae658a02606669bb82e5a1488f4bf4cb
macaguirreok <maquienyt@gmail.com>
2026-05-28 21:27:14 -0300

    f: funcionando , trae los personajes y los muestra en el html

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("Proyecto Harry Potter iniciado");
+
+const url = "https://hp-api.onrender.com/api/characters";
+
+const contenedor = document.getElementById("contenedorPersonajes");
+
+fetch(url)
+.then(response => response.json())
+.then( data =>{
+    data.forEach(personaje => {
+        // .innerHTML agrega html dinamicamente
+      contenedor.innerHTML +=  ` 
+                <div>
+                    <h2>${personaje.name}</h2>
+                    <img src="${personaje.image}" width="200">
+                </div>
+            `;
+    });
+})
+.catch( error => console.log("Error" , error) );
+
+
+
+
+
+
